5efa70d76cabbd8428ac8158cc6088c3067e0eda
lyon <lyon4j@yahoo.com>
2017-08-03 01:33:33 +0800

    change

---
+++ Base/src/main/java/com/c2uol/base/jobs/KeepConnect.java
@@ -0,0 +1,11 @@
+package com.c2uol.base.jobs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KeepConnect {
+
+    public void execute() {
+        System.out.println("Hello");
+    }
+}
